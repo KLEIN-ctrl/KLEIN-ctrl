e1aec031dfd7b9cc4d5505ead54e3bf297bca974
KLEIN-ctrl <82107618+KLEIN-ctrl@users.noreply.github.com>
2021-04-30 22:17:01 +0800

    Create mistake.Demo.java

---
+++ mistake.Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+    public static void main(String[] args) {
+    /*    Object obj=null;
+        System.out.println(obj.hashCode());//”空指针“说明我这个空对象，是不能使用的
+   */
+   /* int arr[]=new int[6];
+    System.out.println(arr[10]);//下标越界
+        */
+      /*  int a=1/0;//算数错误，0不能作为除数
+        */
+        
+    }
+}
