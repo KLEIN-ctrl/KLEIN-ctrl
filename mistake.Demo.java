c05aaafae0883e7de72b50b61f490561b9ac53da
KLEIN-ctrl <82107618+KLEIN-ctrl@users.noreply.github.com>
2021-05-01 22:42:28 +0800

    Update mistake.Demo.java

--- mistake.Demo.java
+++ mistake.Demo.java
@@ -3,11 +3,24 @@ public class Demo {
     /*    Object obj=null;
         System.out.println(obj.hashCode());//”空指针“说明我这个空对象，是不能使用的
    */
+
+
    /* int arr[]=new int[6];
     System.out.println(arr[10]);//下标越界
         */
+
+
+
       /*  int a=1/0;//算数错误，0不能作为除数
         */
-        
+
+
+
+     /*try {...}catch (Exception e){
+         e.printStackTrace();
+     }*/
+        //try{ 有异常的代码}catch（Exception e）{...}
+        //但是ERROR 的错误，是不能用这个修改的，只能重写代码
+
     }
 }
